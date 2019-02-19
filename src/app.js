36020b7be533a9e3fbe386b2de31610978dc7575
HyanCat <hyancat.cn@gmail.com>
2019-02-19 11:01:30 +0800

    Add rate limit.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const Koa = require('koa')
 const app = new Koa()
+const {
+    RateLimiterMemory
+} = require('rate-limiter-flexible')
 const Catcher = require('./catcher')
+
+const rateLimiter = new RateLimiterMemory({
+    points: 100,
+    duration: 60,
+});
+
+app.use(async (ctx, next) => {
+    try {
+        await rateLimiter.consume(ctx.ip)
+        await next()
+    } catch (e) {
+        ctx.status = 429
+        ctx.body = {
+            error: 'Too many requests'
+        }
+    }
+})
 
 app.use(async ctx => {
     const url = ctx.query.webpage
